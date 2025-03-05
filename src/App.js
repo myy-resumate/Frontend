6e72cea837ff1f52a728e0c6665d010f138c94bc
Yeonjoo Lee <joos0207@naver.com>
2025-03-05 23:19:09 +0900

    :sparkles: feat: 로그인 화면 구현 #1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ShopTemplate from "./components/ShopTemplate";
 import Signup from "./components/Sign-up/Signup";
+import LoginForm from "./components/Login/LoginForm";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ShopTemplate />} />
         <Route path="/sign-up" element={<Signup />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </BrowserRouter>
   );
