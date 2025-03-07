08f2a2cbc2c06b5d1b5766e1403b82070e3b7ea9
Yeonjoo Lee <joos0207@naver.com>
2025-03-07 19:22:10 +0900

    :bug: fix: 충돌해결 #2

--- src/components/Sign-up/Signup.js
+++ src/components/Sign-up/Signup.js
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
-<<<<<<< Updated upstream
-import "./Signup.css"; // 스타일 파일 추가
-=======
 import styles from "./Signup.module.css"; // 스타일 파일 추가
 import { Helmet } from 'react-helmet';
->>>>>>> Stashed changes
 
 const Signup = () => {
     const [name, setName] = useState("");
@@ -17,40 +13,12 @@ const Signup = () => {
     };
 
     return (
-        <div className="signup-container">
-            <div className="signup-box">
-                {/* 로고 */}
-                <img src="/hamlogo.png" alt="Resumate Logo" className="signup-logo" />
+        <div>
+            <Helmet>
+                <title>햄매니저 - 홈</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Helmet>
 
-<<<<<<< Updated upstream
-                {/* 입력 필드 */}
-                <input
-                    type="text"
-                    placeholder="이름을 입력해주세요"
-                    className="signup-input"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <input
-                    type="email"
-                    placeholder="사용하실 이메일을 입력해주세요"
-                    className="signup-input"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="사용하실 패스워드를 입력해주세요"
-                    className="signup-input"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-
-                {/* 회원가입 버튼 */}
-                <button className="signup-button" onClick={handleSignup}>
-                    회원가입
-                </button>
-=======
             <div className={styles.signupContainer}>
                 <div className={styles.signupBox}>
                     {/* 로고 */}
@@ -84,7 +52,6 @@ const Signup = () => {
                         회원가입
                     </button>
                 </div>
->>>>>>> Stashed changes
             </div>
         </div>
     );
